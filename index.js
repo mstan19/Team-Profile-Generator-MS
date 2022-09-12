7b5ccd661aba335d9320e379d2e3151c9e0c3579
melissastan <melissastan91@gmail.com>
2022-09-12 13:48:38 -0400

    cleaned up the index.js code

--- index.js
+++ index.js
@@ -5,71 +5,18 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Employee = require("./lib/employee");
 const Intern = require("./lib/intern");
-
-// const generateHTML = ({name,jobIcon,occupation,id,email,GHusername,officeNum,school}) =>
-// `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
-//     <title>Team Profile</title>
-// </head>
-// <body>
-//     <h1>My Team</h1>
-//     //cards
-//     <div class="card" style="width: 18rem">
-//         <div class="card-header">
-//           <h5 class="card-title">${name}</h5>
-//           <h6 class="card-subtitle mb-2 text-muted"><i class=${jobIcon} style="font-size: 25px;"><${occupation}</h6>
-//         </div>
-//         <div class="card-body"></div>
-//             <ul class="list-group">
-//                 <li class="list-group-item"><a href="#" class="card-link">${id}</a></li>
-//                 <li class="list-group-item"><a href="#" class="card-link">${email}</a></li>
-//                 <li class="list-group-item"><a href="#" class="card-link">${GHusername}</a></li>
-//                 <li class="list-group-item"><a href="#" class="card-link">${officeNum}</a></li>
-//                 <li class="list-group-item"><a href="#" class="card-link">${school}</a></li>
-//             </ul>
-//         </div>
-//     </div>
-// </body>
-// </html>`;
-let managerQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the team manager's name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the team manager's id?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the team manager's email?"
-    },
-    {
-        type: "input",
-        name: "officeNum",
-        message: "What is the team manager's office number?"
-    },
-];
+let staff = [];
 
 let selectOccupation = [
     {
         type: "list",
         name: "occupation",
         message: "Which type of team member would you like to add?",
-        choices: [
-            "Intern",
-            "Engineer",
-            "I don't want to add any more team members",
+        choices: ["Intern", "Engineer", "Manager","I don't want to add any more team members",
         ],
     },
 ];
+
 let engineerQuestions = [
     {
         type: "input",
@@ -116,52 +63,101 @@ let internQuestions = [
     }
 ]
 
-function otherMember() {
-    inquirer.prompt(selectOccupation).then((answersselectOcc) => {
-        console.log(answersselectOcc);
-        if (answersselectOcc.occupation === "Engineer") {
-            inquirer.prompt(engineerQuestions).then((answersE) => {
-                // console.log(answersselectOcc.occupation);
+let managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the team manager's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the team manager's email?"
+    },
+    {
+        type: "input",
+        name: "officeNum",
+        message: "What is the team manager's office number?"
+    },
+];
 
-                const engineer = new Engineer(
-                    answersE.name,
-                    answersE.id,
-                    answersE.email,
-                    answersE.GHusername
-                );
-            otherMember();
-            });
-            //go back to questions
-            
-        } else if (answersselectOcc.occupation === "Intern") {
-            inquirer.prompt(internQuestions).then((answersI) => {
-                const intern = new Intern(
-                    answersI.name,
-                    answersI.id,
-                    answersI.email,
-                    answersI.school
-                );
-            otherMember();
-            });
-            
+function mainMenu() {
+    inquirer.prompt(selectOccupation)
+    .then((answersSO) => {
+        if (answersSO.occupation === "Engineer"){
+            console.log(answersSO)
+            engineerMenu();
+        } else if (answersSO.occupation === "Intern"){
+            internMenu();
+        }else if (answersSO.occupation === "Manager"){
+            managerMenu();
         } else {
-            console.log("1test")
-            let htmlTPContent = generateHTML({name,jobIcon,occupation,id,email,GHusername,officeNum,school});
-            console.log("2test")
-            fs.writeFile(`./src/index.html`, htmlTPContent, (err) =>
-                err
-                    ? console.err(
-                        "Failed to generate index.html for the Team Profile file."
-                    )
-                    : console.log(
-                        "Successfully created index.html for the Team Profile file!"
-                    )
-            );
+            writeFile();
         }
     });
 }
 
+function engineerMenu() {
+    inquirer.prompt(engineerQuestions)
+    .then((answersE) => {
+        const engineer = new Engineer(
+            answersE.name,
+            answersE.id,
+            answersE.email,
+            answersE.GHusername
+        );
+        staff.push(engineer);
+        mainMenu();
+    });
+}
+
+function internMenu() {
+    inquirer.prompt(internQuestions)
+    .then((answersI) => {
+        const intern = new Intern(
+            answersI.name,
+            answersI.id,
+            answersI.email,
+            answersI.school
+        );
+        staff.push(intern);
+        mainMenu();
+    });
+}
+
+function managerMenu() {
+    inquirer.prompt(managerQuestions)
+    .then((answersM) => {
+        const manager = new Manager(
+            answersM.name,
+            answersM.id,
+            answersM.email,
+            answersM.officeNum
+        );
+        staff.push(manager);
+        mainMenu();
+    });
+}
+
 function writeFile() {
+
+    fs.writeFile(`./src/index.html`, htmlTPContent, (err) =>
+        err
+            ? console.err(
+                "Failed to generate index.html for the Team Profile file."
+            )
+            : console.log(
+                "Successfully created index.html for the Team Profile file!"
+            )
+    );
+    // console.log("1test")
+    let htmlTPContent = generateHTML({name,jobIcon,occupation,id,email,GHusername,officeNum,school});
+    console.log("2test")
     fs.writeFile(`./src/index.html`, htmlTPContent, (err) =>
         err
             ? console.err(
@@ -172,20 +168,5 @@ function writeFile() {
             )
     );
 }
-// console.log("testing")
-function startQ() {
-    // console.log("startQ")
-    inquirer.prompt(managerQuestions).then((answersM) => {
-        // console.log(answersM);
-        // make instances for new manager
-        const manager = new Manager(
-            answersM.name,
-            answersM.id,
-            answersM.email,
-            answersM.officeNum
-        );
 
-        otherMember();
-    });
-}
-startQ();
+mainMenu();
