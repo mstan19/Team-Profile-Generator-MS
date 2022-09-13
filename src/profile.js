f6c7f7cff19c5666dc132930b970bb9061cd7e3d
melissastan <melissastan91@gmail.com>
2022-09-12 20:08:15 -0400

    adding styling to the generated html

--- src/profile.js
+++ src/profile.js
@@ -1,71 +1,76 @@
 let startStr = `
-<DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="./dist/style.css" />
     <title>Team Profile</title>
 </head>
 <body>
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-          <h1 class="display-4">Fluid jumbotron</h1>
-          <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          <h1 class="greeting display-4 text-center">Meet Our Team!</h1>
         </div>
       </div>
-    <h1>My Team</h1>`
+    <div class="container">
+      <div class="row justify-content-center">`
     
 let endStr =
 `
+</div>
+  </div>
 </body>
 </html>`;
 
 let createManagerStr = (name,jobIcon,occupation,id,email,officeNum) =>
 `
-<div class="card" style="width: 18rem">
+<!-- //Manager -->
+<div class="card col-3 m-1 justify-content-center" >
     <div class="card-header">
-      <h5 class="card-title">${name}</h5>
-      <h6 class="card-subtitle mb-2 text-muted"><i class=${jobIcon} style="font-size: 25px;"><${occupation}</h6>
+      <h5 class="card-title text-center">${name}</h5>
+      <h6 class="card-subtitle mb-2 text-center">${occupation}</h6>
     </div>
-    <div class="card-body"></div>
         <ul class="list-group">
-            <li class="list-group-item"><a href="#" class="card-link">${id}</a></li>
-            <li class="list-group-item"><a href="#" class="card-link">${email}</a></li>
-            <li class="list-group-item"><a href="#" class="card-link">${officeNum}</a></li>
+            <li class="list-group-item common"><a href="#" class="card-link">${id}</a></li>
+            <li class="list-group-item commonTwo"><a href="#" class="card-link">${email}</a></li>
+            <li class="list-group-item other"><a href="#" class="card-link">${officeNum}</a></li>
         </ul>
 </div>
 `
 
 let createEngineerStr = (name,jobIcon,occupation,id,email,GHusername) =>
 ` 
-<div class="card" style="width: 18rem">
-    <div class="card-header">
-      <h5 class="card-title">${name}</h5>
-      <h6 class="card-subtitle mb-2 text-muted"><i class=${jobIcon} style="font-size: 25px;"><${occupation}</h6>
-    </div>
-    <div class="card-body"></div>
-        <ul class="list-group">
-            <li class="list-group-item"><a href="#" class="card-link">${id}</a></li>
-            <li class="list-group-item"><a href="#" class="card-link">${email}</a></li>
-            <li class="list-group-item"><a href="#" class="card-link">${GHusername}</a></li>
-        </ul>
-</div>`
+<!-- //Engineer -->
+        <div class="card col-3 m-1 justify-content-center" >
+            <div class="card-header ">
+              <h5 class="card-title text-center">${name}</h5>
+              <h6 class="card-subtitle mb-2 text-center">${occupation}</h6>
+            </div>
+                <ul class="list-group">
+                    <li class="list-group-item common"><a href="#" class="card-link">${id}</a></li>
+                    <li class="list-group-item commonTwo"><a href="#" class="card-link">${email}</a></li>
+                    <li class="list-group-item other"><a href="#" class="card-link">${GHusername}</a></li>
+                </ul>
+        </div>`
 
 let createInternStr = (name,jobIcon,occupation,id,email,school) =>
 `  
-<div class="card" style="width: 18rem">
-    <div class="card-header">
-      <h5 class="card-title">${name}</h5>
-      <h6 class="card-subtitle mb-2 text-muted"><i class="${jobIcon}" style="font-size: 25px;"><${occupation}</h6>
-    </div>
-    <div class="card-body"></div>
-        <ul class="list-group">
-            <li class="list-group-item"><a href="#" class="card-link">${id}</a></li>
-            <li class="list-group-item"><a href="#" class="card-link">${email}</a></li>
-            <li class="list-group-item"><a href="#" class="card-link">${school}</a></li>
-        </ul>
-</div>`
+<div class="card col-3 m-1 justify-content-center" >
+            <div class="card-header">
+              <h5 class="card-title text-center">${name}</h5>
+              <h6 class="card-subtitle mb-2 text-center">${occupation}</h6>
+            </div>
+                <ul class="list-group">
+                    <li class="list-group-item common"><a href="#" class="card-link">${id}</a></li>
+                    <li class="list-group-item commonTwo"><a href="#" class="card-link">${email}</a></li>
+                    <li class="list-group-item other"><a href="#" class="card-link">${school}</a></li>
+                </ul>
+        </div>`
 
 
 
@@ -85,7 +90,7 @@ function addingStr(allManagers, allinterns, allEngineers) {
         engineerStr += createEngineerStr(engineer.getName(),"", engineer.getRole(), engineer.getId(), engineer.getEmail(),engineer.getGitHub()) 
     }
    
-    return startStr + managerStr + internStr + engineerStr + endStr;
+    return startStr + managerStr + engineerStr + internStr + endStr;
 }
 
 module.exports = addingStr;
